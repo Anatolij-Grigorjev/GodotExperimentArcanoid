ed7291c31d9a9a74f748615722056618e9e01ca8
Anatolij Grigorjev <grigorjev.anatolij@gmail.com>
2018-01-17 20:44:05 +0200

    added main menu transition animations to select stage

--- game/ui/main_menu.gd
+++ game/ui/main_menu.gd
@@ -2,9 +2,12 @@ extends Control
 
 onready var G = get_node("/root/game_state")
 
+onready var level_lbl = get_node("level_panel/level_label")
+onready var anim = get_node("anim")
+
 func _ready():
 	
-
+	refresh_level_lbl()
 	for node in get_children():
 		#launch balls
 		if (node extends preload("res://ball/ball.gd")):
@@ -13,4 +16,12 @@ func _ready():
 		if (node extends preload("res://brick/brick.gd")):
 			node.can_die = false
 	
+func to_level_menu():
+	anim.play("to_stage_select")
+	
+func to_main_menu():
+	anim.play_backwards("to_stage_select")
+	
+func refresh_level_lbl():
+	level_lbl.set_text("# %s" % (G.current_level_idx + 1))
 	
